f9e5836fb7e9472634cd16bde8d67d08f451bec9
Veronchi <zhigunova1995@gmail.com>
2022-01-21 18:42:50 +0300

    implement fetching data for cart items

--- src/context.js
+++ src/context.js
@@ -1,25 +1,37 @@
 import React, { useState, useContext, useReducer, useEffect } from 'react'
 import cartItems from './data'
 import reducer from './reducer'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = 'https://course-api.com/react-useReducer-cart-project'
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  const [cart, setCart] = useState(cartItems)
+  const [cart, setCart] = useState(cartItems);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsLoading(true);
+      const response = await fetch(url);
+      const responceData = await response.json();
+      setIsLoading(false);
+      setCart(responceData);
+    }
+    fetchData();
+  }, [])
 
   return (
     <AppContext.Provider
       value={{
         cart,
+        isLoading
       }}
     >
       {children}
     </AppContext.Provider>
   )
 }
-// make sure use
+
 export const useGlobalContext = () => {
   return useContext(AppContext)
 }
